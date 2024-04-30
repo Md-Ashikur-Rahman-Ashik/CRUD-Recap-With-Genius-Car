12a8e800e502cb38590a4ed5d2c16c838a7d968d
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-04-30 21:19:35 +0600

    Styling header and creating simple banner

---
+++ genius-car-client/src/pages/Banner/Banner.jsx
@@ -0,0 +1,71 @@
+import firstImage from "../../assets/images/banner/1.jpg";
+import secondImage from "../../assets/images/banner/2.jpg";
+import thirdImage from "../../assets/images/banner/3.jpg";
+import fourthImage from "../../assets/images/banner/4.jpg";
+import fifthImage from "../../assets/images/banner/5.jpg";
+import sixthImage from "../../assets/images/banner/6.jpg";
+
+const Banner = () => {
+  return (
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src={firstImage}
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img
+          src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img
+          src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img
+          src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
