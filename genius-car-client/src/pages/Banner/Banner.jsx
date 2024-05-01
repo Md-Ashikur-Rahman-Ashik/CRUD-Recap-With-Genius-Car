f1784c33df7413293b0a55011f78cd203a0495c9
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-01 08:59:34 +0600

    Styled banner component with gradient overlay

--- genius-car-client/src/pages/Banner/Banner.jsx
+++ genius-car-client/src/pages/Banner/Banner.jsx
@@ -7,14 +7,26 @@ import sixthImage from "../../assets/images/banner/6.jpg";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full h-[500px]">
       <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src={firstImage}
-          className="w-full"
-        />
+        <img src={firstImage} className="w-full rounded-xl" />
+        <div className="absolute gap-5 left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] h-full flex items-center">
+          <div className="text-white space-y-7 pl-12 w-1/2">
+            <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div className="flex gap-5">
+              <button className="btn btn-primary">Discover More</button>
+              <button className="btn btn-outline btn-secondary">
+                Secondary
+              </button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
-          <a href="#slide4" className="btn btn-circle">
+          <a href="#slide6" className="btn btn-circle">
             ❮
           </a>
           <a href="#slide2" className="btn btn-circle">
@@ -23,10 +35,7 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src={secondImage}
-          className="w-full"
-        />
+        <img src={secondImage} className="w-full" />
         <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide1" className="btn btn-circle">
             ❮
@@ -37,10 +46,7 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src={thirdImage}
-          className="w-full"
-        />
+        <img src={thirdImage} className="w-full" />
         <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide2" className="btn btn-circle">
             ❮
@@ -51,10 +57,7 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
-        <img
-          src={fourthImage}
-          className="w-full"
-        />
+        <img src={fourthImage} className="w-full" />
         <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide3" className="btn btn-circle">
             ❮
@@ -65,10 +68,7 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide5" className="carousel-item relative w-full">
-        <img
-          src={fifthImage}
-          className="w-full"
-        />
+        <img src={fifthImage} className="w-full" />
         <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide4" className="btn btn-circle">
             ❮
@@ -79,10 +79,7 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide6" className="carousel-item relative w-full">
-        <img
-          src={sixthImage}
-          className="w-full"
-        />
+        <img src={sixthImage} className="w-full" />
         <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide5" className="btn btn-circle">
             ❮
