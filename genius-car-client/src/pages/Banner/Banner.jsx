751a85bfafb4d7b0a3ee49094eac4d0a40602166
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-01 08:05:50 +0600

    Styled header and created simple banner

--- genius-car-client/src/pages/Banner/Banner.jsx
+++ genius-car-client/src/pages/Banner/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
           src={firstImage}
           className="w-full"
         />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide4" className="btn btn-circle">
             ❮
           </a>
@@ -24,10 +24,10 @@ const Banner = () => {
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img
-          src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+          src={secondImage}
           className="w-full"
         />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide1" className="btn btn-circle">
             ❮
           </a>
@@ -38,10 +38,10 @@ const Banner = () => {
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img
-          src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+          src={thirdImage}
           className="w-full"
         />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide2" className="btn btn-circle">
             ❮
           </a>
@@ -52,11 +52,39 @@ const Banner = () => {
       </div>
       <div id="slide4" className="carousel-item relative w-full">
         <img
-          src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
+          src={fourthImage}
           className="w-full"
         />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide5" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide5" className="carousel-item relative w-full">
+        <img
+          src={fifthImage}
+          className="w-full"
+        />
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide6" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide6" className="carousel-item relative w-full">
+        <img
+          src={sixthImage}
+          className="w-full"
+        />
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide5" className="btn btn-circle">
             ❮
           </a>
           <a href="#slide1" className="btn btn-circle">
