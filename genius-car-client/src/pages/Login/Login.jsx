df57f6d55c6d2dd2d2d230d4f3fad17423fa8e85
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-01 11:03:44 +0600

    Created a simple login page
    Integrated firebase

---
+++ genius-car-client/src/pages/Login/Login.jsx
@@ -0,0 +1,58 @@
+import login from "../../assets/images/login/login.svg";
+
+const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content gap-12 flex-col lg:flex-row">
+        <div className="w-1/2">
+          <img src={login} alt="" />
+        </div>
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body" onSubmit={handleLogin}>
+            <h1 className="text-3xl text-center font-bold">Login</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input
+                type="submit"
+                value={"Login"}
+                className="btn btn-primary"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
