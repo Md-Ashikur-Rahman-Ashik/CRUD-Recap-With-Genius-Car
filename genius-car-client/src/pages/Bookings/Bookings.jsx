95c19ba93ca68b11bb8171a9df0376d81d46f331
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-02 08:36:42 +0600

    Displayed bookings and service image
    All data is displayed in a tabular format

--- genius-car-client/src/pages/Bookings/Bookings.jsx
+++ genius-car-client/src/pages/Bookings/Bookings.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import BookingRow from "./BookingRow";
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
@@ -10,11 +11,38 @@ const Bookings = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setBookings(data);
       });
   }, [url]);
 
-  return <div></div>;
+  return (
+    <div>
+      <h2 className="text-5xl text-center">Your bookings: {bookings.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => (
+              <BookingRow key={booking._id} booking={booking}></BookingRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Bookings;
