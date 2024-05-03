a30a4d1307a85c6f8045d63bd161e8724c40c5b9
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-03 09:32:57 +0600

    Project overview completed

--- genius-car-client/src/Routes/PrivateRoute.jsx
+++ genius-car-client/src/Routes/PrivateRoute.jsx
@@ -1,12 +1,16 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  console.log(location.pathname);
 
   if (loading) {
-    return <progress className="progress block mx-auto bg-pink-600 w-56"></progress>;
+    return (
+      <progress className="progress block mx-auto bg-pink-600 w-56"></progress>
+    );
   }
 
   if (user) {
